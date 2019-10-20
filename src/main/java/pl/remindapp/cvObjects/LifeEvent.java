ba65548d4a3d1c19d5890bb9ababf2822595411c
Marcin Cyc <marcinbedcyc@mat.umk.pl>
2019-10-20 20:48:07 +0200

    Implements Comparable

--- src/main/java/pl/remindapp/cvObjects/LifeEvent.java
+++ src/main/java/pl/remindapp/cvObjects/LifeEvent.java
@@ -10,9 +10,27 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LifeEvent implements Serializable {
+public class LifeEvent implements Serializable,Comparable<LifeEvent> {
     private LocalDate begin;
     private LocalDate end;
     private String title;
     private String description;
+
+
+    @Override
+    public int compareTo(LifeEvent o) {
+        if(begin != null ) {
+            if (o.begin != null) {
+                return -begin.compareTo(o.begin);
+            }
+            return -1;
+        }
+        else{
+            if(o.begin != null)
+                return 1;
+            else
+                return 0;
+        }
+
+    }
 }
